59147115e223bf35b1603dc3fbf25ca18e439bfd
Vidhi jain <117736922+vi3003@users.noreply.github.com>
2023-05-02 00:50:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function insert_Row() {
     //Write your code here
-  
+   var table=document.getElementById("sampleTable");
+	var newRow=table.insertRow(0);
+	var leftCell=newRow.insertCell(0);
+	var rightCell=newRow.insertCell(1);
+  leftCell.innerHTML="New Cell1";
+  rightCell.innerHTML="New Cell2";
   
 }
